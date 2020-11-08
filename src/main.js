315b06230e9c0a32a86fee083030a1ca4ae429a2
lauroguedes <leowgweb@gmail.com>
2020-11-08 17:28:36 -0300

    First commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+if (process.env.NODE_ENV === "development") {
+    require("./miragejs/server").makeServer();
+  }
+
 createApp(App).mount('#app')
